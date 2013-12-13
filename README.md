596ce6e7867407fc3acd8a103b5353e382a2ec08
whatever1992 <799758730@qq.com>
2013-12-13 17:41:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+wx-welifemall
+==============
+
+wechat-php-sdk
+微信公众平台php开发包,细化各项接口操作,支持链式调用
---
+++ demo.php
@@ -0,0 +1,46 @@
+<?php
+include "wechat.class.php";
+$options = array(
+        'token'=>'lalala' //填写你设定的key
+    );
+$weObj = new Wechat($options);
+
+// $echoStr = $weObj->valid($return = true);
+// echo $echoStr;
+
+$type = $weObj->getRev()->getRevType();
+
+
+switch($type) {
+    case Wechat::MSGTYPE_TEXT:
+            // $weObj->text("hello, I'm wechat")->reply();
+            $newsData = array(
+                array(
+                    'Title'=>'点击查看今日菜谱',
+                    'Description'=>'',
+                    'PicUrl'=>'http://www.welifemall.com/Upload/20131204101750foc.jpg',
+                    'Url'=>'http://www.domain.com/1.html'
+                    )
+            );
+            $weObj->news($newsData)->reply();
+            exit;
+            break;
+    case Wechat::MSGTYPE_EVENT:
+            break;
+    case Wechat::MSGTYPE_IMAGE:
+            break;
+    default:
+            $weObj->text("help info")->reply();
+}
+
+// //获取菜单操作:
+// $menu = $weObj->getMenu();
+// //设置菜单
+// $newmenu =  array(
+//   "button"=>
+//       array(
+//           array('type'=>'click','name'=>'最新消息','key'=>'MENU_KEY_NEWS'),
+//           array('type'=>'view','name'=>'我要搜索','url'=>'http://www.baidu.com'),
+//           )
+// );
+// $result = $weObj->createMenu($newmenu);

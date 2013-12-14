6465e644d9bbc2936e70b6efdeb0792adf0f6156
whatever1992 <799758730@qq.com>
2013-12-15 04:32:24 +0800

    准备防注入

--- setsql.php
+++ setsql.php
@@ -2,29 +2,46 @@
 // Array ( [1] => on [type] => vegetable [action] => recommond )
 if($_POST[type] && $_POST[action]){
 	// print_r($_POST);
-	
 
-	if($_POST[type] == 'recommond'){
+	if($_POST[action] == 'recommond'){
 		foreach ($_POST as $key => $value) {
-			echo $key;
+			// echo $key;
 			if($key == 'type' || $key == 'action')
 				continue;
 			else{
 				include('conn.php');
+				//MYSQL 不允许在子查询的同时删除原表数据
+				//$query = mysql_query("DELETE FROM `$_POST[type]` WHERE `cno` IN (SELECT `cno` FROM `$_POST[type]` WHERE `type`=0)");
+				$query = mysql_query("SELECT `cno` FROM `$_POST[type]` WHERE `type`=0");
+				while ($row = mysql_fetch_array($query)) {
+					// echo $row[cno];
+					mysql_query("DELETE FROM `$_POST[type]` WHERE `cno`='$row[cno]'");
+				}
+
 				$cno = intval($key);
-				echo "string";
-				$query = mysql_query("INSERT INTO '$_POST[type]' (`cno`,`type`) VALUES ( '$cno', 0)");
+				// echo $cno;
+				$query = mysql_query("INSERT INTO `$_POST[type]` (`cno`,`type`) VALUES ( $cno, 0 )");
 			}
 		}
 	}
-	elseif($_POST[type] == 'normal'){
+	elseif($_POST[action] == 'normal'){
 		foreach ($_POST as $key => $value) {
+			// echo $key;
 			if($key == 'type' || $key == 'action')
 				continue;
 			else{
 				include('conn.php');
+				//MYSQL 不允许在子查询的同时删除原表数据
+				//$query = mysql_query("DELETE FROM `$_POST[type]` WHERE `cno` IN (SELECT `cno` FROM `$_POST[type]` WHERE `type`=0)");
+				$query = mysql_query("SELECT `cno` FROM `$_POST[type]` WHERE `type`=1");
+				while ($row = mysql_fetch_array($query)) {
+					// echo $row[cno];
+					mysql_query("DELETE FROM `$_POST[type]` WHERE `cno`='$row[cno]'");
+				}
+
 				$cno = intval($key);
-				$query = mysql_query("INSERT INTO '$_POST[type]' (`cno`,`type`) VALUES ( '$cno', 1)");
+				// echo $cno;
+				$query = mysql_query("INSERT INTO `$_POST[type]` (`cno`,`type`) VALUES ( $cno, 1)");
 			}
 		}
 	}
--- sort_cai.php
+++ sort_cai.php
@@ -1,8 +1,8 @@
 <?
 if($_GET){
 	include 'conn.php';
-	$type = $_GET[type];
-	switch ($type) {
+
+	switch ($_GET[type]) {
 		case 'restaurant':
 			$type = '餐厅';
 			break;
@@ -18,16 +18,28 @@ if($_GET){
 		default:
 			break;
 	}
+
+	switch ($_GET[action]) {
+		case 'recommond':
+			$action = '设置推荐';
+			break;
+		case 'normal':
+			$action = '设置';
+			break;
+		default:
+			break;
+	}
 	$query = mysql_query("SELECT * from `cai` WHERE `type` = '$type' ");
 	// $get = array();
+	
+	echo $type . $action . "<br />";
 
 	echo "<form action='setsql.php' method='post'>";
 	while($row = mysql_fetch_array($query)){
 		// array_push($get,$row);
 		echo '<input type="checkbox" name="' . $row[cno] . '" /><a href="cai.php?cno=' . $row[cno] . '">' . $row[cname] . '</a> <br />';
 	}
-	$type = $_GET[type];
-	echo "<input type='hidden' name='type' value='" . $type . "'>"; // 
+	echo "<input type='hidden' name='type' value='" . $_GET[type] . "'>"; // 
 	echo "<input type='hidden' name='action' value='" . $_GET[action] . "'>"; //
 	echo "<input type='submit' value='设置'>";
 	echo "</form>";
